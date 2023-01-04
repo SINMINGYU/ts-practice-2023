89e6e7b75fc8a46ad76828f5c151b7d22f389077
SinMinGyu <smgms7513@gmail.com>
2023-01-04 15:27:10 +0900

    chapter1_01_notCompile

---
+++ dist/practice.js
@@ -0,0 +1,14 @@
+"use strict";
+let count = 0;
+count += 1;
+count = "갑자기 분위기 문자열";
+const message = "hello world";
+const done = true;
+const numbers = [1, 2, 3];
+const messages = ["hello", "world"];
+messages.push(1);
+let mightBeUndefined = undefined;
+let nullableNumber = null;
+let color = "red";
+color = "yellow";
+color = "green";
